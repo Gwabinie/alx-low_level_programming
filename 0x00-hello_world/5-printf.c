9c23f684237bcfdac547d148776247c3d907bc82
Lungelo <Loongelow@gmail.com>
2023-07-14 00:01:52 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed newline
+ *
+ */
+
+int main(void)
+
+{ 
+	printf("with proper grammr, but the outcome is a piece of art,\n");
+	return (0);
